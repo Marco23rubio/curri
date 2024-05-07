dae0f29a7792a0e8e8872aa469582bc849a26c90
Marco23rubio <marcoantonio23rubio@gmail.com>
2024-05-07 10:57:09 -0700

    cambios de la experiencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -12,7 +14,9 @@ import { RouterOutlet } from '@angular/router';
     CommonModule,
     FormsModule,
     InputTextModule,
-    RouterOutlet
+    RouterOutlet,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
 })
 export class AppModule { }
