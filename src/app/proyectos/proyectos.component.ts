beb87620f4ecfe04b3371a48a6caefc4370ebee7
Marco23rubio <marcoantonio23rubio@gmail.com>
2024-05-14 16:57:06 -0700

    Se agregan rutas a repos

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -23,14 +23,14 @@ export class ProyectosComponent {
       descripcion: 'El código de la página web que estás viendo,desarrollado con Angular y tailwindcss, enfocado en un buen diseño y una buena experiencia de usuario.',
       titulo: 'Curriculum Vitae',
       imagen: 'assets/curri1.jpg',
-      link: 'https://github.com/Marco23rubio/Cursos'
+      link: 'https://github.com/Marco23rubio/curri'
     },
     {
       descripcion: 'Proyecto en python para obtener el punto óptimo de los paneles solares para extraer la mayor cantidad de energía a través de cálculos matemáticos y físicos.',
       titulo: 'Paneles solares',
       md :'col-span-2',
       imagen: 'assets/paneles4.jpeg',
-      link: 'https://github.com/Marco23rubio/Cursos'
+      link: 'https://github.com/Marco23rubio/Solares'
     },
     {
       descripcion: 'Ejercicios enfocados en el análisis de datos con Python, utilizando librerías como Pandas, Numpy, Matplotlib, Seaborn, Scikit-learn, etc.',
@@ -43,20 +43,20 @@ export class ProyectosComponent {
       descripcion: 'Resolución de diferentes problemas con Python para la plataforma HackerRank, enfocados en la lógica de programación.',
       titulo: 'HackerRank',
       imagen: 'assets/hr4.png',
-      link: 'https://github.com/Marco23rubio/Cursos'
+      link: 'https://github.com/Marco23rubio/HackerRank'
     },
     {
       descripcion: 'Repositorio enfocado con el transcurso de aprendizaje en python con diferentes dificultades para mejora del entendimiento del lenguaje.',
       titulo: 'Aprendizaje de python',
       imagen: 'assets/py.webp',
-      link: 'https://github.com/Marco23rubio/Cursos'
+      link: 'https://github.com/Marco23rubio/Python'
     },
     {
       descripcion: 'Aqui se encuentran mis inicios , enfocado principalmente en la lógica basada en diagramas de flujo y principios básicos de programación.',
       titulo: 'Fundamentos de programación',
       md :'col-span-2',
       imagen: 'assets/prog3.jpg',
-      link: 'https://github.com/Marco23rubio/Cursos'
+      link: 'https://github.com/Marco23rubio/fundamentos-de-programacion'
     },
   ]
 
