40f4f346221b2f08f1d774facf32be48a67ea91a
Marco23rubio <marcoantonio23rubio@gmail.com>
2024-05-13 18:07:37 -0700

    se crea ciclo en proyectos

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -1,13 +1,67 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-proyectos',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './proyectos.component.html',
   styleUrl: './proyectos.component.css'
 })
 export class ProyectosComponent {
 
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  items = [
+    {
+      descripcion: 'Texto prueba chiquito',
+      titulo: 'Proyecto 1',
+      imagen: 'assets/mailbox.png',
+      link: 'https://github.com/Marco23rubio/Cursos'
+    },
+    {
+      descripcion: 'Texto prueba',
+      titulo: 'Proyecto 2',
+      md :'col-span-2',
+      imagen: 'assets/excel.png',
+      link: 'https://github.com/Marco23rubio/Cursos'
+    },
+    {
+      descripcion: 'Texto prueba',
+      titulo: 'Proyecto 3',
+      md :'col-span-2',
+      imagen: 'assets/excel.png',
+      link: 'https://github.com/Marco23rubio/Cursos'
+    },
+    {
+      descripcion: 'Texto prueba chiquito',
+      titulo: 'Proyecto 4',
+      imagen: 'assets/mailbox.png',
+      link: 'https://github.com/Marco23rubio/Cursos'
+    },
+    {
+      descripcion: 'Texto prueba chiquito',
+      titulo: 'Proyecto 5',
+      imagen: 'assets/mailbox.png',
+      link: 'https://github.com/Marco23rubio/Cursos'
+    },
+    {
+      descripcion: 'Texto prueba',
+      titulo: 'Proyecto 6',
+      md :'col-span-2',
+      imagen: 'assets/excel.png',
+      link: 'https://github.com/Marco23rubio/Cursos'
+    },
+  ]
+
+  enviarAlMenu() {
+    this.router.navigate(['/menu']);
+  }
   
 }
