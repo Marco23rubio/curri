cf33dfd58106d131df735b9752838450e64630dc
Marco23rubio <marcoantonio23rubio@gmail.com>
2024-05-29 13:16:47 -0700

    se cambia img de assets por links

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -21,19 +21,19 @@ export class CursosComponent {
     {
         'titulo': 'Curso de AMIB',
         'descripcion': 'Certificación del AMIB en figura 3, te valida como Asesor de Inversiones y ofrece autorización para representar clientes en operaciones bursátiles',
-        'imagen': 'assets/amib.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012161/amib_zqpjpz.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/tree/master/AMIB'
     },
     {
         'titulo': 'Carrera en Blockchain',
         'descripcion': 'Conocimientos para entender y contribuir en la tecnología Blockchain para el sector financiero, además de estrategias de inversión para criptomonedas y tokens digitales',
-        'imagen': 'assets/bitcoin.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012161/bitcoin_yfyfsi.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/tree/master/Carrera%20Blockchain'
     },
     {
         'titulo': 'Análisis técnico de mercados financieros', 
         'descripcion': 'Certificación especializada en el análisis técnico de empresas y cálculos estadísticos para predecir el comportamiento de las acciones en el futuro',
-        'imagen': 'assets/stock-market.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012158/stock-market_rfw9ri.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/blob/master/Analisis_tecnico.jpeg'
     }
 ]
@@ -42,19 +42,19 @@ itemscol2 = [
     {
         'titulo': 'Creación de Startups',
         'descripcion': 'Cursos enfocados en la creación de empresas emergentes así como en la administración de las mismas',
-        'imagen': 'assets/rocket.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012158/rocket_irq2ye.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/tree/master/Carrera%2C%20creacion%20de%20Startups'
     },
     {
         'titulo': 'Competencias gerenciales',
         'descripcion': 'Curso especializado en ofrecer las herramientas para liderar equipos de trabajo y tomar decisiones en situaciones de crisis',
-        'imagen': 'assets/team-management.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012159/team-management_vxpepp.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/blob/master/Competencias_gerenciales.jpeg'
     },
     {
         'titulo': 'Iniciación y Planificación de Proyectos',
         'descripcion': 'Materia con los principios fundamentales de la gestión de proyectos, sobre todo en la parte inicial de los mismos',
-        'imagen': 'assets/project-management.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012156/project-management_t7kjnr.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/blob/master/Iniciaci%C3%B3n_planificacionDeProyectos.pdf'
     }
 ]
@@ -63,25 +63,25 @@ itemscol3 = [
     {
         'titulo': 'Carrera de bases de datos',
         'descripcion': 'Conjunto de cursos enfocados al manejo de BD, la interpretación de datos y la toma de decisiones más óptimas en base a estos mismos',
-        'imagen': 'assets/database-storage.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012148/database-storage_wxel4e.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/tree/master/Bases%20de%20datos'
     },
     {
         'titulo': 'Fundamentos de programación',
         'descripcion': 'Itinerario enfocado en las bases de la programación y creación de algoritmos para la resolución de problemas',
-        'imagen': 'assets/coding-language.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012148/coding-language_lg6ymf.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/tree/master/Fundamentos%20de%20programaci%C3%B3n'
     },
     {
         'titulo': 'Excel avanzado',
         'descripcion': 'Cursos enfocados en el uso de Excel para el análisis de datos y financiero',
-        'imagen': 'assets/excel.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012148/excel_j2gyre.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/tree/master/Excel'
     },
     {
         'titulo': 'Engineering Project Management',
         'descripcion': 'Enfocado en la correcta administración de proyectos de ingeniería, como administrar riesgos, recursos, control de calidad y tiempos de entrega',
-        'imagen': 'assets/engineering.png',
+        'imagen': 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012148/engineering_v7scrs.png',
         'ruta': 'https://github.com/Marco23rubio/Cursos/blob/master/EngineeringProjectManagement.pdf'
     }
 ]
--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -22,40 +22,40 @@ export class ProyectosComponent {
     {
       descripcion: 'El código de la página web que estás viendo, desarrollado con Angular y TailwindCSS, enfocado en un buen diseño y una buena experiencia de usuario.',
       titulo: 'Currículum Vitae',
-      imagen: 'assets/curri1.jpg',
+      imagen: 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012148/curri1_aauw3o.jpg',
       link: 'https://github.com/Marco23rubio/curri'
     },
     {
       descripcion: 'Proyecto en Python para obtener el punto óptimo de los paneles solares para extraer la mayor cantidad de energía a través de cálculos matemáticos y físicos.',
       titulo: 'Paneles solares',
       md: 'col-span-2',
-      imagen: 'assets/paneles4.jpeg',
+      imagen: 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012154/paneles4_cnzedo.jpg',
       link: 'https://github.com/Marco23rubio/Solares'
     },
     {
       descripcion: 'Ejercicios enfocados en el análisis de datos con Python, utilizando librerías como Pandas, Numpy, Matplotlib, Seaborn, Scikit-learn, etc.',
       titulo: 'Google Colab y Jupyter Notebooks',
       md: 'col-span-2',
-      imagen: 'assets/jupyter2.png',
+      imagen: 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012151/jupyter2_swytgs.png',
       link: 'https://github.com/Marco23rubio/JupyterNotebooks'
     },
     {
       descripcion: 'Resolución de diferentes problemas con Python para la plataforma HackerRank, enfocados en la lógica de programación.',
       titulo: 'HackerRank',
-      imagen: 'assets/hr4.png',
+      imagen: 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012150/hr4_yzudjs.png',
       link: 'https://github.com/Marco23rubio/HackerRank'
     },
     {
       descripcion: 'Repositorio enfocado en el transcurso de aprendizaje en Python con diferentes dificultades para mejorar el entendimiento del lenguaje.',
       titulo: 'Aprendizaje de Python',
-      imagen: 'assets/py.webp',
+      imagen: 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012156/py_ybmsas.webp',
       link: 'https://github.com/Marco23rubio/Python'
     },
     {
       descripcion: 'Aquí se encuentran mis inicios, enfocados principalmente en la lógica basada en diagramas de flujo y principios básicos de programación.',
       titulo: 'Fundamentos de programación',
       md: 'col-span-2',
-      imagen: 'assets/prog3.jpg',
+      imagen: 'https://res.cloudinary.com/dvzucfpbk/image/upload/v1717012155/prog3_uky7qr.jpg',
       link: 'https://github.com/Marco23rubio/fundamentos-de-programacion'
     }
   ];
